de8668e4131da212a74840756a8c46fd887b52fe
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-10 23:11:04 +0100

    recently added books

--- js/index.js
+++ js/index.js
@@ -75,3 +75,16 @@ const toggleBackButton = () => {
 };
 
 setActive();
+
+// var elem = document.querySelector(".carousel");
+// var flkty = new Flickity(elem, {
+//   // options
+//   cellAlign: "left",
+//   contain: true,
+// });
+
+// // element argument can be a selector string
+// //   for an individual element
+// var flkty = new Flickity(".carousel", {
+//   // options
+// });
