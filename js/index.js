a2657d728c80d95239e55fddfd925bb55e875652
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-12 09:58:48 +0100

    fix input suggestions

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
+
+
 const setActive = () => {
   if (window.screen.width > 1200) {
     document.getElementById("sidebar").classList.add("active");
-
-    // document.getElementById("sidebar").style.width = "350px";
-    // document.getElementsByClassName("main-content")[0].style.marginLeft =
-    //   "350px";
   } else {
     document.getElementById("sidebar").classList.remove("active");
   }
+};
+
+const setSearchPlaceholder = () => {
+  if (window.screen.width < 1000) {
+    const input = document.getElementById("input");
+    input.placeholder = "Search";
+  } else {
+    const input = document.getElementById("input");
+    input.placeholder = "Search books, genres, authors, etc.";
+  }
+};
+
+const toggleSearchResults = () => {
+  console.log("=sss");
+  const searchSugesstions = document.querySelector(".nav__search__suggestions");
+  searchSugesstions.classList.toggle("shown_suggestions");
 };
 
 window.addEventListener("click", (e) => {
@@ -31,32 +45,67 @@ window.addEventListener("click", (e) => {
 
 window.addEventListener("resize", () => {
   setActive();
+  setSearchPlaceholder();
+  //   toggleSearch(window.screen.width);
+
+  //   if (window.screen.width > 660) {
+  //     const navLeft = document.querySelector(".nav__left");
+  //     navLeft.style.display = "flex";
+
+  //     const navSearch = document.querySelector(".nav__search");
+  //     navSearch.style.display = "flex";
+
+  //     const navLinks = document.querySelector(".nav__links");
+  //     navLinks.style.display = "flex";
+
+  //     const navButton = document.querySelector(".nav__button");
+  //     navButton.style.display = "none";
+  //   }
+  //   else {
+  //     const navSearch = document.querySelector(".nav__search");
+  //     navSearch.style.display = "none";
+  //   }
 });
 
 document.getElementById("mobile-search");
 
 const toggleNav = () => {
   const sidebar = document.getElementById("sidebar").classList.add("active");
-  console.log("---");
   if (window.screen.width > 1200) {
     document.getElementById("sidebar").style.width = "350px";
     document.querySelector("main-content").style.marginLeft = "350px";
   }
 };
 
-const toggleSearch = () => {
-  const navLeft = document.querySelector(".nav__left");
-  navLeft.style.display = "none";
+const toggleSearch = (width = 660) => {
+  if (width <= 660) {
+    const navLeft = document.querySelector(".nav__left");
+    navLeft.style.display = "none";
 
-  const navSearch = document.querySelector(".nav__search");
-  navSearch.style.display = "flex";
-  navSearch.className = "nav__search nav__search__mobile";
+    const navSearch = document.querySelector(".nav__search");
+    navSearch.style.display = "flex";
+    navSearch.className = "nav__search nav__search__mobile";
 
-  const navLinks = document.querySelector(".nav__links");
-  navLinks.style.display = "none";
+    const navLinks = document.querySelector(".nav__links");
+    navLinks.style.display = "none";
 
-  const navButton = document.querySelector(".nav__button");
-  navButton.style.display = "flex";
+    const navButton = document.querySelector(".nav__button");
+    navButton.style.display = "flex";
+  }
+  //   else {
+  //     const navLeft = document.querySelector(".nav__left");
+  //     navLeft.style.display = "flex";
+
+  //     const navSearch = document.querySelector(".nav__search");
+  //     navSearch.style.display = "none";
+  //     // navSearch.className = "nav__search nav__search__mobile";
+
+  //     const navLinks = document.querySelector(".nav__links");
+  //     navLinks.style.display = "flex";
+
+  //     const navButton = document.querySelector(".nav__button");
+  //     navButton.style.display = "none";
+  //   }
 };
 
 const toggleBackButton = () => {
@@ -74,7 +123,7 @@ const toggleBackButton = () => {
 };
 
 setActive();
-
+setSearchPlaceholder();
 // var elem = document.querySelector(".carousel");
 // var flkty = new Flickity(elem, {
 //   // options
