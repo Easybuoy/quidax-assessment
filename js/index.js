33a48520312ba36980a3ac0d6f2a246fe6c43c65
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-12 09:03:48 +0100

    fix input suggestions

--- js/index.js
+++ js/index.js
@@ -5,6 +5,8 @@ const setActive = () => {
     // document.getElementById("sidebar").style.width = "350px";
     // document.getElementsByClassName("main-content")[0].style.marginLeft =
     //   "350px";
+  } else {
+    document.getElementById("sidebar").classList.remove("active");
   }
 };
 
@@ -18,8 +20,7 @@ window.addEventListener("click", (e) => {
       if (!(e.target.alt === "Menu")) {
         if (window.screen.width > 1200) {
           document.getElementById("sidebar").style.width = "0";
-          document.getElementsByClassName("main-content")[0].style.marginLeft =
-            "0";
+          document.querySelector(".main-content").style.marginLeft = "0";
         }
 
         document.getElementById("sidebar").classList.remove("active");
@@ -39,37 +40,36 @@ const toggleNav = () => {
   console.log("---");
   if (window.screen.width > 1200) {
     document.getElementById("sidebar").style.width = "350px";
-    document.getElementsByClassName("main-content")[0].style.marginLeft =
-      "350px";
+    document.querySelector("main-content").style.marginLeft = "350px";
   }
 };
 
 const toggleSearch = () => {
-  const navLeft = document.getElementsByClassName("nav__left")[0];
+  const navLeft = document.querySelector(".nav__left");
   navLeft.style.display = "none";
 
-  const navSearch = document.getElementsByClassName("nav__search")[0];
+  const navSearch = document.querySelector(".nav__search");
   navSearch.style.display = "flex";
   navSearch.className = "nav__search nav__search__mobile";
 
-  const navLinks = document.getElementsByClassName("nav__links")[0];
+  const navLinks = document.querySelector(".nav__links");
   navLinks.style.display = "none";
 
-  const navButton = document.getElementsByClassName("nav__button")[0];
+  const navButton = document.querySelector(".nav__button");
   navButton.style.display = "flex";
 };
 
 const toggleBackButton = () => {
-  const navLeft = document.getElementsByClassName("nav__left")[0];
+  const navLeft = document.querySelector(".nav__left");
   navLeft.style.display = "flex";
 
-  const navSearch = document.getElementsByClassName("nav__search")[0];
+  const navSearch = document.querySelector(".nav__search");
   navSearch.style.display = "none";
 
-  const navLinks = document.getElementsByClassName("nav__links")[0];
+  const navLinks = document.querySelector(".nav__links");
   navLinks.style.display = "flex";
 
-  const navButton = document.getElementsByClassName("nav__button")[0];
+  const navButton = document.querySelector(".nav__button");
   navButton.style.display = "none";
 };
 
